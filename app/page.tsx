76bd6a6bcfc6b95315328cff8ea51888bfa2bf49
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-04-21 19:47:59 +0200

    feature: create home compenent, fetch all products from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+import Product from "@/components/Product";
+import Stripe from "stripe";
+
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  });
+
+  const products = await stripe.products.list();
+  console.log(products);
+
+  /*
+  - Use Promise.all() to fetch all product prices at once instead of fetching them one by one in a loop.
+  - Instead of waiting for each price to be fetched before moving on to the next product.
+   */
+  const productWithPrices = await Promise.all(
+    // Fetch the prices for each product by using products ID
+    products.data.map(async (product) => {
+      const prices = await stripe.prices.list({ product: product.id });
+      return {
+        id: product.images,
+        name: product.name,
+        price: prices.data[0].unit_amount,
+        image: product.images[0],
+        currency: prices.data[0].currency,
+      };
+    })
+  );
+
+  return productWithPrices;
+};
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
   return (
     <main>
-      <h1 className="text-lg">Hello Next 13 🔥</h1>
+      {products.map((product) => (
+        <Product {...product} />
+      ))}
     </main>
   );
 }
