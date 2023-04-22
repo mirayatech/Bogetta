57b7ffb56b20d3586e90714c650a7b1aa89b6ec4
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-04-22 07:31:40 +0200

    add: another property for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        fluid: "repeat(auto-fit, minmax(20rem,1fr))",
+      },
       colors: {
         // general
-        light: "#EAE7DE",
+        light: "#fff",
         middle: "#666666",
         dark: "#151412",
         // Brown shades
