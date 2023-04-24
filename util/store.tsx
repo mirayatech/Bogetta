582eb608bce32e81b08a29273c6c48ab8d53815c
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-04-24 09:11:31 +0200

    add: add add product function to useCartStore

--- util/store.tsx
+++ util/store.tsx
@@ -6,6 +6,7 @@ type CartState = {
   isOpen: boolean;
   cart: AddCartType[];
   toggleCart: () => void;
+  addProduct: (item: AddCartType) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -14,6 +15,23 @@ export const useCartStore = create<CartState>()(
       cart: [],
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
+      addProduct: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          if (existingItem) {
+            const updatedCart = state.cart.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity! + 1 };
+              }
+              return cartItem;
+            });
+            return { cart: updatedCart };
+          } else {
+            return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
+        }),
     }),
     { name: "cart-store" }
   )
