43062fc616483317aae96f8f637ec6ef251b7897
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-04-27 16:49:27 +0200

    feature: payment intent, checkout system done, create checkout component

---
+++ app/components/Checkout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useCartStore } from "@/util/store";
+import { useEffect, useState } from "react";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+export default function Checkout() {
+  const cartStore = useCartStore();
+  const [clientSecret, setClientSecret] = useState(0);
+  useEffect(() => {
+    // Create payment intent when checkout page loads.
+    // Generate new payment intent ID every time user goes to checkout page.
+    // Save generated ID for later use in case of cart update and re-checkout.
+
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        items: cartStore.cart,
+        payment_intent_id: cartStore.paymentIntent,
+      }),
+    });
+  }, []);
+  return <div></div>;
+}
--- util/store.tsx
+++ util/store.tsx
@@ -8,6 +8,10 @@ type CartState = {
   toggleCart: () => void;
   addProduct: (item: AddCartType) => void;
   removeProduct: (item: AddCartType) => void;
+  paymentIntent: string;
+  onCheckout: string;
+  setPaymentIntent: (val: string) => void;
+  setOnCheckout: (val: string) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -15,6 +19,8 @@ export const useCartStore = create<CartState>()(
     (set) => ({
       cart: [],
       isOpen: false,
+      paymentIntent: "",
+      onCheckout: "string",
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
       addProduct: (item) =>
         set((state) => {
@@ -55,7 +61,10 @@ export const useCartStore = create<CartState>()(
             return { cart: filteredCart };
           }
         }),
+      setPaymentIntent: (val) => set((state) => ({ paymentIntent: val })),
+      setOnCheckout: (val) => set((state) => ({ onCheckout: val })),
     }),
+
     { name: "cart-store" }
   )
 );
