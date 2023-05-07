96d9595c174aa37c3f8638576abb103d70da957d
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-05-07 13:50:05 +0200

    feature: create and add styling to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,15 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth/next";
 import Image from "next/image";
 
-import formatPrice from "@/util/price-format";
+import React from "react";
+import Container from "../components/container";
+import { formatPrice } from "@/util/price-format";
+import { prisma } from "@/util/prisma";
 
 export const revalidate = 0;
 
 const fetchOrders = async () => {
-  const prisma = new PrismaClient();
   const user = await getServerSession(authOptions);
 
   if (!user) {
@@ -16,7 +17,7 @@ const fetchOrders = async () => {
   }
   const orders = await prisma.order.findMany({
     //@ts-ignore
-    where: { userId: user?.user?.id, status: "complete" },
+    where: { userId: user?.user?.id },
     include: { products: true },
   });
   return orders;
@@ -25,42 +26,99 @@ const fetchOrders = async () => {
 export default async function Dashboard() {
   const orders = await fetchOrders();
 
-  if (orders === null)
-    return <div>You need to be logged in to see yout orders</div>;
+  const formatDate = (date: any) => {
+    const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+    return date.toLocaleDateString("en-GB", options);
+  };
+
+  if (orders === null) {
+    return (
+      <div className="flex flex-col gap-5 items-center justify-center h-screen text-center m-auto">
+        <Image
+          alt="done"
+          height={200}
+          width={200}
+          src={"/login.png"}
+          priority={true}
+          className="w-auto"
+        />
+        <h1 className="text-xl">You need to be logged in to see your orders</h1>
+      </div>
+    );
+  }
 
   if (orders.length === 0) {
     return (
-      <div>
-        <h1>No orders placed</h1>
+      <div className="flex flex-col items-center justify-center h-screen m-auto gap-5 w-[500px]">
+        <Image
+          alt="done"
+          height={150}
+          width={150}
+          src={"/box.png"}
+          priority={true}
+        />
+        <h1 className="my-5 flex justify-between text-2xl">
+          No orders placed.
+        </h1>
       </div>
     );
   }
 
   return (
-    <div>
-      {orders.length === 0 ? <h1>No orders</h1> : <h1>Your orders</h1>}
+    <Container>
+      <div className="flex justify-center items-center m-auto py-5 h-full p-2">
+        <div className="flex flex-col justify-center h-full m-auto gap-3 w-[1000px]">
+          {orders.map((order) => (
+            <div key={order.id} className="rounded-sm p-8 my-4  bg-cardColor">
+              <div className="flex justify-between mb-5 border-b border--textColor">
+                <span className="pb-5">
+                  <span className="font-semibold pr-1">Order reference:</span>{" "}
+                  {order.id}
+                </span>
 
-      <div>
-        {orders.map((order) => (
-          <div key={order.id}>
-            <h2>Order Refrerence: {order.id}</h2>
-            <span>Time: {new Date(order.createdDate).toString()}</span>
-            <span>Total:{formatPrice(order.amount)}</span>
-            <div>{order.status}</div>
-            <div>
-              {order.products.map((product) => (
-                <div key={product.id}>
-                  <span>{product.name}</span>
-                  {/* @ts-ignore */}
-                  <Image height={100} width={100} src={product.image} alt="." />
-                  <span>{formatPrice(product.unit_amount)}</span>
-                  <span>Quantity: {product.quantity}</span>
-                </div>
-              ))}
+                <span className="pb-5">
+                  <span className="font-semibold pr-1">Date:</span>{" "}
+                  {formatDate(new Date(order.createdDate))}
+                </span>
+              </div>
+              <div className="grid grid-cols-4 items-center gap-5 text-sm">
+                {order.products.map((product) => (
+                  <React.Fragment key={product.id}>
+                    <span>
+                      {" "}
+                      <span className="font-semibold pr-1">Product:</span>{" "}
+                      {product.name}
+                    </span>
+                    <span>
+                      <span className="font-semibold pr-1">Price:</span>{" "}
+                      {formatPrice(product.unit_amount)}
+                    </span>
+                    <span>
+                      <span className="font-semibold pr-1">Quantity:</span>{" "}
+                      {product.quantity}
+                    </span>{" "}
+                    <div className="w-[60px]">
+                      <Image
+                        src={product.image!}
+                        width={60}
+                        height={60}
+                        alt={product.name}
+                        priority={true}
+                        className="w-full object-fill"
+                      />
+                    </div>
+                  </React.Fragment>
+                ))}
+              </div>
+              <p className="flex justify-between mt-5 pt-5 border-t border--textColor">
+                <span className="font-semibold">Total:</span>
+
+                <span> {formatPrice(order.amount)}</span>
+              </p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
